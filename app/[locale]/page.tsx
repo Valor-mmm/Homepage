415df670fcac0b155eb6330be445990ba3a3bf4d
Valor-mmm <v_dev/github@slmail.me>
2024-05-21 07:37:55 +0200

    chore(cleanup): remove page dummy translation

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,8 @@
-import { useTranslations } from 'next-intl';
 import { CV } from '@/features/cv/cv';
 
 export default function Home() {
-  const t = useTranslations('Index');
-
   return (
     <>
-      <h1 className="text-2xl">{t('title')}</h1>
       <CV />
     </>
   );
